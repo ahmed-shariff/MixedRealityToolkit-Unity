23176145baab2f4199f22683b015ac5e58b95e1e
Shariff Faleel <shariff.mfa@outlook.com>
2023-11-08 17:08:11 -0800

    Setup tests

---
+++ org.mixedrealitytoolkit.uxcore/Tests/Runtime/VirtualizedScrollRectListTests.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Mixed Reality Toolkit Contributors
+// Licensed under the BSD 3-Clause
+
+// Disable "missing XML comment" warning for tests. While nice to have, this documentation is not required.
+#pragma warning disable CS1591
+
+using System.Collections;
+using MixedReality.Toolkit.Input.Tests;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+// using MixedReality.Toolkit.UX.Experimental;
+
+namespace MixedReality.Toolkit.UX.Runtime.Tests
+{
+    /// <summary>
+    /// Tests for the VirtualizedScrollRectList.
+    /// </summary>
+    public class VirtualizedScrollRectListTests: BaseRuntimeInputTests
+    {
+
+    }
+}
+#pragma warning restore CS1591
